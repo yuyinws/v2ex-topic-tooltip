9530754e1a05d008811de820bca14e375cb094bc
yuyinws <lyc657508366@gmail.com>
2023-02-13 10:09:37 +0800

    chore: v0.0.1

--- dist/v2ex-topic-tooltip.user.js
+++ dist/v2ex-topic-tooltip.user.js
@@ -10,8 +10,19 @@
 
 (function() {
   "use strict";
+  function isDarkMode() {
+    return !!document.querySelector(".Night");
+  }
   const tooltipEl = document.createElement("div");
-  const CLOSE_TIMEOUT = 100;
+  const loadingEl = document.createElement("div");
+  loadingEl.innerHTML = "Fetching...";
+  loadingEl.style.cssText = `
+    text-align: center;
+    margin-top: 40px;
+    color: #666;
+`;
+  tooltipEl.appendChild(loadingEl);
+  const CLOSE_TIMEOUT = 300;
   let timer = 0;
   let isTooltipShow = false;
   function createTooltip() {
@@ -20,20 +31,21 @@
     width: 500px;
     min-height: 100px;
     max-height: 400px;
-    background: #fff;
+    background: ${isDarkMode() ? "#25303e" : "#fff"};
     padding: 20px;
-    border: 1px solid #e4e7ed;
+    border: 1px solid ${isDarkMode() ? "#1f2227" : "#e4e7ed"};
     box-shadow: 0px 0px 12px rgba(0, 0, 0, .12);
     overflow: auto;
     z-index: 100;
     border-radius: 4px;
     top: 0;
     display: none;
+    align-items: center;
   `;
-    tooltipEl.addEventListener("mouseover", () => {
+    tooltipEl.addEventListener("mouseenter", () => {
       clearTimeout(timer);
     });
-    tooltipEl.addEventListener("mouseout", () => {
+    tooltipEl.addEventListener("mouseleave", () => {
       hideTooltip();
     });
     document.body.appendChild(tooltipEl);
@@ -49,29 +61,44 @@
     tooltipEl.style.display = "block";
     isTooltipShow = true;
   }
+  async function getContent(tid) {
+    try {
+      tooltipEl.innerHTML = "";
+      tooltipEl.appendChild(loadingEl);
+      let domString = "";
+      if (sessionStorage.getItem(`v2ex-${tid}`)) {
+        domString = sessionStorage.getItem(`v2ex-${tid}`);
+      } else {
+        const response = await fetch(`https://www.v2ex.com/t/${tid}`);
+        domString = await response.text();
+        sessionStorage.setItem(`v2ex-${tid}`, domString);
+      }
+      const domParser = new DOMParser();
+      const dom = domParser.parseFromString(domString, "text/html");
+      const content = dom.querySelector("#Main > .box");
+      tooltipEl.innerHTML = "";
+      tooltipEl.append(content);
+    } catch (error) {
+      console.log("🚀 v2ex-topic-tooltip error", error);
+    }
+  }
   function main() {
-    document.querySelectorAll(".item_title > a").forEach((el) => {
-      el.addEventListener("mouseover", (event) => {
+    document.querySelectorAll(".item_title").forEach((el) => {
+      el.addEventListener("mouseenter", (event) => {
+        var _a;
+        clearTimeout(timer);
         if (!isTooltipShow) {
           const pageX = event.pageX;
           const pageY = event.pageY;
           tooltipEl.style.transform = `translate(${pageX}px, ${pageY}px)`;
-          const href = el.getAttribute("href");
+          const href = (_a = el.querySelector("a")) == null ? void 0 : _a.getAttribute("href");
           const tid = href == null ? void 0 : href.match(/\d+/g)[0];
-          fetch(`https://www.v2ex.com/t/${tid}`).then((response) => {
-            response.text().then((res) => {
-              const domParser = new DOMParser();
-              const dom = domParser.parseFromString(res, "text/html");
-              const content = dom.querySelector(".topic_content");
-              tooltipEl.innerHTML = "";
-              tooltipEl.append(content);
-            });
-          });
-          clearTimeout(timer);
           showTooltip();
+          if (tid)
+            getContent(tid);
         }
       });
-      el.addEventListener("mouseout", () => {
+      el.addEventListener("mouseleave", () => {
         hideTooltip();
       });
     });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
+import { isDarkMode } from './utils'
+
 const tooltipEl = document.createElement('div')
-const CLOSE_TIMEOUT = 100
+const loadingEl = document.createElement('div')
+
+loadingEl.innerHTML = 'Fetching...'
+loadingEl.style.cssText = `
+    text-align: center;
+    margin-top: 40px;
+    color: #666;
+`
+
+tooltipEl.appendChild(loadingEl)
+
+const CLOSE_TIMEOUT = 300
 let timer = 0
 let isTooltipShow = false
 
@@ -9,21 +22,22 @@ function createTooltip() {
     width: 500px;
     min-height: 100px;
     max-height: 400px;
-    background: #fff;
+    background: ${isDarkMode() ? '#25303e' : '#fff'};
     padding: 20px;
-    border: 1px solid #e4e7ed;
+    border: 1px solid ${isDarkMode() ? '#1f2227' : '#e4e7ed'};
     box-shadow: 0px 0px 12px rgba(0, 0, 0, .12);
     overflow: auto;
     z-index: 100;
     border-radius: 4px;
     top: 0;
     display: none;
+    align-items: center;
   `
-  tooltipEl.addEventListener('mouseover', () => {
+  tooltipEl.addEventListener('mouseenter', () => {
     clearTimeout(timer)
   })
 
-  tooltipEl.addEventListener('mouseout', () => {
+  tooltipEl.addEventListener('mouseleave', () => {
     hideTooltip()
   })
   document.body.appendChild(tooltipEl)
@@ -42,31 +56,49 @@ function showTooltip() {
   isTooltipShow = true
 }
 
+async function getContent(tid: string) {
+  try {
+    tooltipEl.innerHTML = ''
+    tooltipEl.appendChild(loadingEl)
+    let domString = ''
+    if (sessionStorage.getItem(`v2ex-${tid}`)) {
+      domString = sessionStorage.getItem(`v2ex-${tid}`)!
+    }
+    else {
+      const response = await fetch(`https://www.v2ex.com/t/${tid}`)
+      domString = await response.text()
+      sessionStorage.setItem(`v2ex-${tid}`, domString)
+    }
+
+    const domParser = new DOMParser()
+    const dom = domParser.parseFromString(domString, 'text/html')
+    const content = dom.querySelector('#Main > .box')
+    tooltipEl.innerHTML = ''
+    tooltipEl.append(content!)
+  }
+  catch (error) {
+    console.log('🚀 v2ex-topic-tooltip error', error)
+  }
+}
+
 function main() {
-  document.querySelectorAll('.item_title > a').forEach((el) => {
-    el.addEventListener('mouseover', (event) => {
+  document.querySelectorAll('.item_title').forEach((el) => {
+    el.addEventListener('mouseenter', (event) => {
+      clearTimeout(timer)
+
       if (!isTooltipShow) {
         const pageX = (event as MouseEvent).pageX
         const pageY = (event as MouseEvent).pageY
         tooltipEl.style.transform = `translate(${pageX}px, ${pageY}px)`
-        const href = el.getAttribute('href')
+        const href = el.querySelector('a')?.getAttribute('href')
         const tid = href?.match(/\d+/g)![0]
-
-        fetch(`https://www.v2ex.com/t/${tid}`).then((response) => {
-          response.text().then((res) => {
-            const domParser = new DOMParser()
-            const dom = domParser.parseFromString(res, 'text/html')
-            const content = dom.querySelector('.topic_content')
-            tooltipEl.innerHTML = ''
-            tooltipEl.append(content!)
-          })
-        })
-        clearTimeout(timer)
         showTooltip()
+        if (tid)
+          getContent(tid)
       }
     })
 
-    el.addEventListener('mouseout', () => {
+    el.addEventListener('mouseleave', () => {
       hideTooltip()
     })
   })
